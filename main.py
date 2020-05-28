c1474d47d078f5d7087ab0d5d238c9257e503fd5
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-28 06:07:55 -0700

    🤔 Stream fibonacci numbers to the client
    ? Why do we need to use a "StreamingResponse"?
    https://github.com/tiangolo/fastapi/issues/1167#issuecomment-610467266
    
    ? Is the infinite loop appropriate?
    https://channels.readthedocs.io/en/latest/topics/consumers.html?highlight=broadcast
    Or is it more appropriate to use something **like** a "Consumer"
    "...called whenever an event happens, rather than making you write an event loop."
    https://github.com/encode/uvicorn/issues/388
    
    ? How do we check when the connection has been closed by the client?
    Are we sending into a void?
    Also in https://github.com/encode/uvicorn/issues/388#issue-467737174
    Sample php has connection_aborted()
    https://github.com/mdn/dom-examples/blob/master/server-sent-events/sse.php
    
    ? How do we implement a heartbeat to keep the connection open?
    "A heartbeat is kept to keep the connections alive
    (WebSockets also do that) and to detect dropped clients (since dropped
    connections can only be detected when data is pushed to them)."
    https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+from time import sleep
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+
+def streamFibonacci(num):
+    sumBefore, sum = 0, 1
+    while True:
+        yield f'data: {str(sum)}\n\n'
+        sumBefore, sum = sum, sum + sumBefore
+        sleep(1)
+
 @app.get("/")
 def home():
     return {"Hi!": "Pax!"}
+
+@app.get("/sse")
+def sse():
+    return StreamingResponse(streamFibonacci(500), media_type="text/event-stream")
