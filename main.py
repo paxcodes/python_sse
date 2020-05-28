43b979a0cbc5e9fca5241f382b575902c463bcaf
Margret `Pax` Williams <pax.margret@gmail.com>
2020-05-28 05:25:39 -0700

    :sparkles: Add CORSMiddleware so that html file can connect to the server

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 @app.get("/")
 def home():
     return {"Hi!": "Pax!"}
