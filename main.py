9bfce1bb99e4b0843d59fcd884526bd65ebc5512
Margret `Pax` Williams <pax.margret@gmail.com>
2020-06-01 16:50:30 -0700

    Run server through python script so we can debug in VS code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+import uvicorn
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
@@ -23,3 +25,7 @@ def home():
 @app.get("/sse")
 def sse():
     return StreamingResponse(streamFibonacci(500), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='trace', reload=True)
